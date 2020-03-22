337b83a055fba6ff17b1c17b3f57ac1672e2ce20
Dominik Kuenkele <dominik.kuenkele@outlook.com>
2020-03-22 18:09:39 +0100

    Initial commit

---
+++ src/main/java/com/kuenkele/handballstatistics/HandballstatisticsApplication.java
@@ -0,0 +1,13 @@
+package com.kuenkele.handballstatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandballstatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandballstatisticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuenkele/handballstatistics/HandballstatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuenkele.handballstatistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandballstatisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
